dc9ed91786942fa95fdb453e0653a207cb6501eb
fahad314-beep <fatifahad314@gmail.com>
2026-05-11 17:14:37 +0000

    serve static files

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(require('express').static(__dirname));
 
 function checkEmail(email) {
   return new Promise((resolve) => {
